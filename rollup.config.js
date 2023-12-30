cea7131ec3d48c06ff7eaa59493d0e0c5c7ffdd6
Alisios Corporation <126966048+alisioscorp@users.noreply.github.com>
2023-12-30 23:39:35 +0100

    Add files via upload

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,27 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonJs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-import json from '@rollup/plugin-json';
+import postCss from 'rollup-plugin-postcss';
 import terser from "@rollup/plugin-terser";
 import dts from 'rollup-plugin-dts';
 
 import pkg from './package.json' assert { type: 'json' };
-const { name, homepage, version, dependencies, peerDependencies } = pkg;
+const { name, homepage, version, dependencies } = pkg;
 
 const umdConf = {
   format: 'umd',
-  name: 'ThreeGlobe',
-  globals: { three: 'THREE' },
+  name: 'Globe',
   banner: `// Version ${version} ${name} - ${homepage}`
 };
+
 export default [
   {
-    external: ['three'],
     input: 'src/index.js',
     output: [
       {
         ...umdConf,
         file: `dist/${name}.js`,
-        sourcemap: true,
+        sourcemap: true
       },
       { // minify
         ...umdConf,
@@ -33,10 +32,10 @@ export default [
       }
     ],
     plugins: [
-      json({ compact: true }),
       resolve(),
       commonJs(),
-      babel({ exclude: 'node_modules/**', babelHelpers: "bundled" })
+      postCss(),
+      babel({ exclude: 'node_modules/**', babelHelpers: 'bundled' })
     ]
   },
   { // ES module
@@ -47,10 +46,10 @@ export default [
         file: `dist/${name}.mjs`
       }
     ],
-    external: [...Object.keys(dependencies), ...Object.keys(peerDependencies)],
+    external: Object.keys(dependencies),
     plugins: [
-      json({ compact: true }),
-      babel({ babelHelpers: "bundled" })
+      postCss(),
+      babel({ babelHelpers: 'bundled' })
     ]
   },
   { // expose TS declarations
