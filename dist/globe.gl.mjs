3f33658d78ac1d136f99cf26ca53da7a43705265
Alisios Corporation <126966048+alisioscorp@users.noreply.github.com>
2024-01-19 00:35:27 +0100

    Update globe.gl.mjs

--- dist/globe.gl.mjs
+++ dist/globe.gl.mjs
@@ -1,6 +1,10 @@
-const express = require("express");
+/*const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+*/
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.get("/", (req, res) => res.type('html').send(html));
 
